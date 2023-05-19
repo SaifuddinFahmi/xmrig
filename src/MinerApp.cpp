c568bad901b3ee981d64e14bb1bb42c9c4d8c919
SaifuddinFahmi <engineringf@gmail.com>
2023-05-20 00:45:47 +0700

    Create MinerApp.cpp

---
+++ src/MinerApp.cpp
@@ -0,0 +1,125 @@
+/* XMRig
+ * Copyright (c) 2018-2022 SChernykh   <https://github.com/SChernykh>
+ * Copyright (c) 2016-2022 XMRig       <https://github.com/xmrig>, <support@xmrig.com>
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+  * Additional permission under GNU GPL version 3 section 7
+  *
+  * If you modify this Program, or any covered work, by linking or combining
+  * it with OpenSSL (or a modified version of that library), containing parts
+  * covered by the terms of OpenSSL License and SSLeay License, the licensors
+  * of this Program grant you additional permission to convey the resulting work.
+ */
+
+#include <cstdlib>
+#include <uv.h>
+
+
+#include "MinerApp.h"
+#include "backend/cpu/Cpu.h"
+#include "base/io/Console.h"
+#include "base/io/log/Log.h"
+#include "base/io/log/Tags.h"
+#include "base/io/Signals.h"
+#include "core/config/Config.h"
+#include "core/Controller.h"
+#include "Summary.h"
+#include "version.h"
+
+
+xmrig::MinerApp::MinerApp()
+{
+    m_controller = std::make_shared<Controller>();
+}
+
+
+xmrig::MinerApp::~MinerApp()
+{
+    Cpu::release();
+}
+
+
+int xmrig::MinerApp::exec()
+{
+    if (!m_controller->isReady()) {
+        LOG_EMERG("no valid configuration found, try https://xmrig.com/wizard");
+
+        return 2;
+    }
+
+    m_signals = std::make_shared<Signals>(this);
+
+    int rc = m_controller->init();
+    if (rc != 0) {
+        return rc;
+    }
+
+    if (!Log::isBackground()) {
+        m_console = std::make_shared<Console>(this);
+    }
+
+    Summary::print(m_controller.get());
+
+    if (m_controller->config()->isDryRun()) {
+        LOG_NOTICE("%s " WHITE_BOLD("OK"), Tags::config());
+
+        return 0;
+    }
+
+    m_controller->start();
+
+    rc = uv_run(uv_default_loop(), UV_RUN_DEFAULT);
+    uv_loop_close(uv_default_loop());
+
+    return rc;
+}
+
+
+void xmrig::MinerApp::onConsoleCommand(char command)
+{
+    if (command == 3) {
+        LOG_WARN("%s " YELLOW("Ctrl+C received, exiting"), Tags::signal());
+        close();
+    }
+    else {
+        m_controller->execCommand(command);
+    }
+}
+
+
+void xmrig::MinerApp::onSignal(int signum)
+{
+    switch (signum)
+    {
+    case SIGHUP:
+    case SIGTERM:
+    case SIGINT:
+        return close();
+
+    default:
+        break;
+    }
+}
+
+
+void xmrig::MinerApp::close()
+{
+    m_signals.reset();
+    m_console.reset();
+
+    m_controller->stop();
+
+    Log::destroy();
+}
